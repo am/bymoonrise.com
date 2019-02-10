57a90b2d7b879d0a1ac4e0cbed9698665d501069
antonio miranda <antsmiranda@gmail.com>
2019-02-11 00:01:33 +0100

    Add moonrise time

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import suncalc from 'suncalc'
 
-const times = suncalc.getMoonTimes(new Date(), 41.3851, 2.1734);
-console.log(times.rise)
+const { rise } = suncalc.getMoonTimes(new Date(), 41.3851, 2.1734);
+const noteEl = document.querySelector('#moonrise-text')
+const hour = rise.toLocaleString(
+  'en-US',
+  { hour: 'numeric', minute: 'numeric', hour12: true }
+)
+noteEl.innerText = `barcelona - moonrise at ${hour}`
